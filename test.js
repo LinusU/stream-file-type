0ad5bf24d68b14c58d6e8aa3fd85202a9d9b3f2a
Linus Unnebäck <linus@folkdatorn.se>
2018-10-18 09:05:32 +0200

    🌹 Bump mocha & standard

--- test.js
+++ test.js
@@ -19,8 +19,8 @@ describe('stream-file-type', () => {
     detector.on('file-type', fileType => { emitted = fileType })
 
     return detector.fileTypePromise()
-      .then(type => assert.deepEqual(type, { ext: 'mid', mime: 'audio/midi' }))
-      .then(() => assert.deepEqual(emitted, { ext: 'mid', mime: 'audio/midi' }))
+      .then(type => assert.deepStrictEqual(type, { ext: 'mid', mime: 'audio/midi' }))
+      .then(() => assert.deepStrictEqual(emitted, { ext: 'mid', mime: 'audio/midi' }))
   })
 
   it('should pass the input as-is', () => {
@@ -28,7 +28,7 @@ describe('stream-file-type', () => {
     const detector = new FileType()
 
     return hasha.fromStream(input.pipe(detector), { algorithm: 'sha1' })
-      .then(hash => assert.equal(hash, '02cd79ed1d5f07eef736f79122aa89e6fe4f0d4b'))
+      .then(hash => assert.strictEqual(hash, '02cd79ed1d5f07eef736f79122aa89e6fe4f0d4b'))
   })
 
   it('should handle files larger than 4100 bytes', () => {
@@ -41,7 +41,7 @@ describe('stream-file-type', () => {
     detector.on('file-type', fileType => { emitted = fileType })
 
     return detector.fileTypePromise()
-      .then(type => assert.deepEqual(type, { ext: 'woff2', mime: 'application/font-woff' }))
-      .then(() => assert.deepEqual(emitted, { ext: 'woff2', mime: 'application/font-woff' }))
+      .then(type => assert.deepStrictEqual(type, { ext: 'woff2', mime: 'application/font-woff' }))
+      .then(() => assert.deepStrictEqual(emitted, { ext: 'woff2', mime: 'application/font-woff' }))
   })
 })
