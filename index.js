7fdc6611bea8d8643e4d4b9ef3fb9eaf1fec6a70
Blake B <blakebuell7@gmail.com>
2021-11-03 04:09:58 -0400

    Fix stream stalling on some files.

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ export default class FileType extends Transform {
 
   _flush (cb) {
     if (this[kStream] != null) {
-      this[kStream].end(() => cb(null))
+      this[kStream].end()
+      cb(null)
     } else {
       cb(null)
     }
